5546927864660a7117125aaf8835b1c57dbc4c59
young_ogi <duddhrdl2727@naver.com>
2023-10-16 21:14:17 +0900

    url에 도서 제목 넣어서 요청 시 depth3와 제목 코사인 유사도를 사용해서 도서 목록 반환하는 코드 작성 완료

--- sebook_backend/book/BookRecommender.py
+++ sebook_backend/book/BookRecommender.py
@@ -17,12 +17,20 @@ class BookRecommender:
 
     def recommend_books(self, user_book):
         
-        idx = next(i for i, book in enumerate(self.data) if book["title"] == user_book)
+        idx = next((i for i, book in enumerate(self.data) if book["title"] == user_book), None)
         
         if idx is None:
             return []
         
-        sim_scores = list(enumerate(self.cosine_sim[idx]))
+        # 동일한 depth3 카테고리의 책만 선택
+        same_depth3_books_indices = [i for i, book in enumerate(self.data) if book["depth3"] == self.data[idx]["depth3"]]
+        
+        sim_scores = [(i, score) for i, score in enumerate(self.cosine_sim[idx]) if i in same_depth3_books_indices]
+    
         sim_scores.sort(key=lambda x: x[1], reverse=True)
         
+        # 만약 추천 가능한 도서가 없다면 빈 리스트 반환
+        if not sim_scores:
+            return []
+
         return [self.data[i[0]]['title'] for i in sim_scores[1:11]]
