32970001b2568e03f43e3bc63132948b2e195eee
young_ogi <duddhrdl2727@naver.com>
2023-11-12 14:35:40 +0900

    [사용자] 회원가입 기능 완료

--- sebook_backend/user/urls.py
+++ sebook_backend/user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import GetUser
+from .views import GetUser,UserSignUp
 
 urlpatterns = [
     path('memberSearch', GetUser.as_view(), name='memberSearch'),
+    path('memberReg', UserSignUp.as_view(), name='memberReg'),
 ]
--- sebook_backend/user/views.py
+++ sebook_backend/user/views.py
@@ -4,7 +4,7 @@ from .models import User
 from .serializers import UserSerializer
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
-
+from rest_framework import status
 class GetUser(APIView):
     @swagger_auto_schema(manual_parameters=[
         openapi.Parameter('userNum', openapi.IN_QUERY, description="User number", type=openapi.TYPE_INTEGER)
@@ -21,3 +21,13 @@ class GetUser(APIView):
 
         serializer = UserSerializer(user)
         return Response(serializer.data, status=200)
+
+class UserSignUp(APIView):
+    @swagger_auto_schema(request_body=UserSerializer)
+    def post(self, request):
+
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
