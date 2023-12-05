69e414d5298fac57bad4a5479351fb11aa4035e1
young_ogi <duddhrdl2727@naver.com>
2023-12-06 02:06:24 +0900

    [최종 수정] 페이징 되돌리기

--- sebook_backend/community/views.py
+++ sebook_backend/community/views.py
@@ -47,7 +47,7 @@ class CommunityListRead(APIView):
     def get(self, request):
 
         all_community = Community.objects.all().order_by('-registDate_community')
-        paginator = Paginator(all_community, 5)
+        paginator = Paginator(all_community, 4)
 
         page_number = request.query_params.get('page')
         page_obj = paginator.get_page(page_number)
@@ -162,7 +162,7 @@ class SearchCommunityByAuthor(APIView):
         communities = Community.objects.filter(isbn13_community__author__icontains=author).order_by('-registDate_community')
 
         # 페이징 처리
-        paginator = Paginator(communities, 5)
+        paginator = Paginator(communities, 4)
         page_number = request.query_params.get('page')
         page_obj = paginator.get_page(page_number)
 
@@ -188,7 +188,7 @@ class SearchCommunityByTitle(APIView):
             return Response({"message": f"No results found for title: {title}"}, status=404)
 
         # 페이징 처리
-        paginator = Paginator(communities, 5)
+        paginator = Paginator(communities, 4)
         page_number = request.query_params.get('page')
         page_obj = paginator.get_page(page_number)
 
